7ad030c6fe5ede43703a19ec2b4072a966e35f18
zekageri <zekageri@hotmail.com>
2021-11-07 12:58:05 +0100

    inline icons

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     checkScrollPosition();
     initMobileNavEvent();
     initContacts();
-    getIcons();
+    insertIcons();
     projectDescription_Event();
 });
 
